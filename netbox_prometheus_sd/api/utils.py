5cbc9feaecf79a9fe6f0eec166cd6d61dac91787
Graeme Moss <graeme.moss@nscale.com>
2025-03-12 11:49:45 +0000

    code fixing

--- netbox_prometheus_sd/api/utils.py
+++ netbox_prometheus_sd/api/utils.py
@@ -185,12 +185,12 @@ def extract_full_location(obj,labels: LabelDict):
         None
     """
     string = ""
-    string += obj.site.name + "/"
+    string += f"{obj.site.name}/"
     ancestors = obj.location.get_ancestors()
     for ancestor in ancestors:
-        string += str(ancestor) + "/"
-    string += obj.location.name + "/"
-    if obj.rack != None:
+        string += f"{str(ancestor)}/"
+    string += f"{obj.location.name}/"
+    if obj.rack is not None:
         string += obj.rack.name
 
     labels["full_location"] = string
